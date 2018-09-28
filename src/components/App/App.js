717af33cfcaf4756580b82dd319597b83a1ff820
Max Tsai <Max.Tsai@tdc.innovasolutions.int>
2018-09-28 10:52:51 +0800

    added app.js & setting.json

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 
 import Date from '../Date/Date';
+import Content from '../Content/Content';
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <Date />
+        <Content />
       </div>
     )
   }
