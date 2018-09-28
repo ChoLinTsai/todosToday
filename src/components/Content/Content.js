7db28bfe5ff285e251d6203eb484bc8fcfe0fb99
Max Tsai <Max.Tsai@tdc.innovasolutions.int>
2018-09-28 10:41:33 +0800

    Added Content cmp and test vs code PR extension

---
+++ src/components/Content/Content.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Content extends Component {
+  render() {
+    return (
+      <div>
+        <h2>This is h2 from content cmp</h2>
+      </div>
+    )
+  }
+}
