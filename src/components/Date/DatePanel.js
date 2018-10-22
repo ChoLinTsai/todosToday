efc1667a5cd5bf59ee188e88a4a364a2efcf2040
Cho Lin Tsai <max19851113@gmail.com>
2018-10-22 21:58:40 +0800

    date func added

--- src/components/Date/DatePanel.js
+++ src/components/Date/DatePanel.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
-import DatePicker from 'react-datepicker';
-import moment from 'moment';
-
+import DatePicker from "react-datepicker";
+import moment from "moment";
+import "react-datepicker/dist/react-datepicker.css";
+import { Button } from "reactstrap";
 import "./datepanel.scss";
 
 function GetDate() {
@@ -13,14 +14,60 @@ function GetDate() {
 }
 
 export default class DatePanel extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      startDate: moment()
+    };
+  }
+
+  handleChange(date) {
+    this.setState({
+      startDate: date
+    });
+  }
+
+  clickChange(num) {
+    if (num > 0)
+      return this.setState({
+        startDate: this.state.startDate.add(num, "d")
+      });
+    if (num < 0)
+      return this.setState({
+        startDate: this.state.startDate.add(num, "d")
+      });
+  }
+
   render() {
+    console.log(888, this.state.startDate);
     return (
       <div className="datePanel">
-        <div className="prevDate">&laquo;</div>
+        <Button
+          onClick={() => this.clickChange(-1)}
+          color="info"
+          className="prevDate"
+          size="md"
+        >
+          Previous
+        </Button>
         <div className="today">
           <GetDate />
+          <DatePicker
+            dateFormat="YYYY/MM/DD"
+            selected={this.state.startDate}
+            onChange={date => this.handleChange(date)}
+            className="dateInput"
+          />
         </div>
-        <div className="nextDate">&raquo;</div>
+        <Button
+          onClick={() => this.clickChange(1)}
+          color="info"
+          className="nextDate"
+          size="md"
+        >
+          Next
+        </Button>
       </div>
     );
   }
